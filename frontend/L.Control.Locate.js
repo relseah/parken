ba57c7936f4ec6c0986c0b52542523c181f03425
David Häsler <david2997924@gmail.com>
2022-04-07 16:28:05 +0200

    Implement control to mark geolocation on map

---
+++ frontend/L.Control.Locate.js
@@ -0,0 +1,62 @@
+L.Control.Locate = L.Control.extend({
+    position: null,
+    coordinates: function() {
+        return L.latLng(this.position.coords.latitude, this.position.coords.longitude);
+    },
+    _button: document.createElement('button'),
+    _icon: document.createElement('span'),
+    _map: null,
+    _marker: null,
+    options: {
+        iconClass: 'fa-solid fa-location-dot fa-2x',
+        animationClass: 'fa-fade',
+        markerOptions: {
+            radius: 5,
+            color: 'steelblue',
+            fillOpacity: 1
+        }
+    },
+    initialize: function(options) {
+        L.setOptions(this, options);
+        this._icon.classList = this.options.iconClass;
+        this._button.appendChild(this._icon);
+    },
+    onAdd: function(map) {
+        this._map = map;
+        L.DomEvent.on(this._button, 'click', this.requestPosition, this);
+        return this._button;
+    },
+    onRemove: function() {
+        L.DomEvent.off(_button, 'click', this.requestPosition, this);
+        this._map = null;
+    },
+    requestPosition: function() {
+        this.startAnimation();
+        navigator.geolocation.getCurrentPosition(this._processPosition.bind(this), null, {enableHighAccuracy: true});
+    },
+    _processPosition: async function(position)  {
+        if (this._marker != null) {
+            this._marker.remove();
+        }
+        this.position = position;
+        this._marker = L.circleMarker(this.coordinates(), this.options.markerOptions).addTo(map);
+        map.flyTo(this.coordinates());
+        this.stopAnimation();
+    },
+    startAnimation: function() {
+        if (!this._isAnimated) {
+            this._icon.classList.add(this.options.animationClass);
+            this._isAnimated = true;
+        }
+    },
+    stopAnimation: function() {
+        if(this._isAnimated) {
+            this._icon.classList.remove(this.options.animationClass)
+            this._isAnimated = false;
+        }
+    }
+});
+
+L.control.locate = function(options) {
+    return new L.Control.Locate(options);
+};
--- frontend/parken.js
+++ frontend/parken.js
@@ -1,16 +1,13 @@
 function initializeMap() {
-    let map = L.map('map').setView([49.40, 8.67], 13);
+    let map = L.map('map').setView([49.41032, 8.69707], 13);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'
-    }).addTo(map);    
+    }).addTo(map);
     return map
-}
-
-function processPosition(map, position) {
-    L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
 }
 
 let map = initializeMap();
 if ('geolocation' in navigator) {
-    navigator.geolocation.watchPosition(position => processPosition(map, position));
+    let locateControl = L.control.locate({position: 'bottomright'}).addTo(map);
+    locateControl.requestPosition();
 }
