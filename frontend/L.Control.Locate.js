b400a59c75a51358977a22fd436b37112e86096a
David Häsler <david2997924@gmail.com>
2022-04-08 02:36:54 +0200

    Improve on error handling and request geolocation permission more elegantly

--- frontend/L.Control.Locate.js
+++ frontend/L.Control.Locate.js
@@ -1,62 +1,82 @@
 L.Control.Locate = L.Control.extend({
-    position: null,
-    coordinates: function() {
-        return L.latLng(this.position.coords.latitude, this.position.coords.longitude);
-    },
-    _button: document.createElement('button'),
-    _icon: document.createElement('span'),
-    _map: null,
-    _marker: null,
-    options: {
-        iconClass: 'fa-solid fa-location-dot fa-2x',
-        animationClass: 'fa-fade',
-        markerOptions: {
-            radius: 5,
-            color: 'steelblue',
-            fillOpacity: 1
-        }
-    },
-    initialize: function(options) {
-        L.setOptions(this, options);
-        this._icon.classList = this.options.iconClass;
-        this._button.appendChild(this._icon);
-    },
-    onAdd: function(map) {
-        this._map = map;
-        L.DomEvent.on(this._button, 'click', this.requestPosition, this);
-        return this._button;
-    },
-    onRemove: function() {
-        L.DomEvent.off(_button, 'click', this.requestPosition, this);
-        this._map = null;
-    },
-    requestPosition: function() {
-        this.startAnimation();
-        navigator.geolocation.getCurrentPosition(this._processPosition.bind(this), null, {enableHighAccuracy: true});
-    },
-    _processPosition: function(position)  {
-        if (this._marker != null) {
-            this._marker.remove();
-        }
-        this.position = position;
-        this._marker = L.circleMarker(this.coordinates(), this.options.markerOptions).addTo(map);
-        map.flyTo(this.coordinates());
-        this.stopAnimation();
-    },
-    startAnimation: function() {
-        if (!this._isAnimated) {
-            this._icon.classList.add(this.options.animationClass);
-            this._isAnimated = true;
-        }
-    },
-    stopAnimation: function() {
-        if(this._isAnimated) {
-            this._icon.classList.remove(this.options.animationClass)
-            this._isAnimated = false;
-        }
-    }
+	position: null,
+	_firstPosition: true,
+	coordinates: function () {
+		return L.latLng(
+			this.position.coords.latitude,
+			this.position.coords.longitude
+		);
+	},
+	_geolocationWatchId: null,
+	_button: document.createElement("button"),
+	_icon: document.createElement("span"),
+	// Avoid conflict with _map property of parent class.
+	_boundMap: null,
+	_marker: null,
+	options: {
+		iconClass: "fa-solid fa-location-dot fa-2x",
+		animationClass: "fa-fade",
+		markerOptions: {
+			radius: 5,
+			color: "steelblue",
+			fillOpacity: 1,
+		},
+		geolocationOptions: {
+			enableHighAccuracy: true,
+			timeout: 10000,
+		},
+	},
+	initialize: function (options) {
+		L.setOptions(this, options);
+		this._icon.classList = this.options.iconClass;
+		this._button.appendChild(this._icon);
+	},
+	addTo(map) {
+		L.Control.prototype.addTo.call(this, map);
+		this.requestPosition();
+	},
+	onAdd: function (map) {
+		this._boundMap = map;
+		L.DomEvent.on(this._button, "click", this.onClick, this);
+		return this._button;
+	},
+	onRemove: function () {
+		L.DomEvent.off(this._button, "click", this.onClick, this);
+		navigator.geolocation.clearWatch(this._geolocationWatchId);
+		this._boundMap = null;
+	},
+	onClick: function () {
+		if (this.position != null) this.flyToPosition();
+	},
+	requestPosition: function () {
+		this._icon.classList.add(this.options.animationClass);
+		this._geolocationWatchId = navigator.geolocation.watchPosition(
+			this._processPosition.bind(this),
+			this._handleError.bind(this),
+			this.options.geolocationOptions
+		);
+	},
+	flyToPosition: function () {
+		this._boundMap.flyTo(this.coordinates());
+	},
+	_processPosition: function (position) {
+		if (this._marker != null) this._marker.remove();
+		this.position = position;
+		this._marker = L.circleMarker(
+			this.coordinates(),
+			this.options.markerOptions
+		).addTo(this._boundMap);
+		if (this._firstPosition) {
+			this.flyToPosition();
+			this._icon.classList.remove(this.options.animationClass);
+			this._firstPosition = false;
+		}
+	},
+	_handleError: function () {
+		this.remove();
+	},
 });
 
-L.control.locate = function(options) {
-    return new L.Control.Locate(options);
+L.control.locate = function (options) {
+	return new L.Control.Locate(options);
 };
--- frontend/parken.js
+++ frontend/parken.js
@@ -1,13 +1,25 @@
 function initializeMap() {
-    let map = L.map('map').setView([49.41032, 8.69707], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'
-    }).addTo(map);
-    return map
+	let map = L.map("map").setView([49.41032, 8.69707], 13);
+	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+		attribution:
+			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	}).addTo(map);
+	return map;
+}
+
+function initializeLocateControl(map) {
+	L.control.locate({ position: "bottomright" }).addTo(map);
 }
 
 let map = initializeMap();
-if ('geolocation' in navigator) {
-    let locateControl = L.control.locate({position: 'bottomright'}).addTo(map);
-    locateControl.requestPosition();
-}
+if ("geolocation" in navigator)
+	if ("permissions" in navigator)
+		navigator.permissions.query({ name: "geolocation" }).then((status) => {
+			if (status.state != "denied") {
+				initializeLocateControl(map);
+			}
+			status.onchange = function () {
+				if (this.state == "prompt") initializeLocateControl(map);
+			};
+		});
+	else initializeLocateControl();
