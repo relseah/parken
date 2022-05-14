cd2c4fc468a140ce07fb83b824492fe3822b0ab5
David Häsler <david2997924@gmail.com>
2022-05-15 01:53:59 +0200

    Add configuration file of live reload utility to .gitignore

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 rmdir dist /s /q
 cd backend
-go build -o ..\dist\parken.exe .\cli
+go build -o ..\dist\parken.exe
 cd ..
 robocopy . dist config.json parkings.json 
 robocopy frontend dist\frontend /s
