1030cfd2cf17441303f05f59a90fbdbfb03385ca
David Häsler <david2997924@gmail.com>
2022-07-30 10:37:37 +0200

    Update .gitignore

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ rmdir dist /s /q
 cd backend
 go build %* -o ..\dist\parken.exe .\cmd
 cd ..
-robocopy . dist config.json payload.json
+robocopy . dist config.json dummy.json
 robocopy frontend dist\frontend /s
 :: /ndl /nfl
 if errorlevel 8 (exit /b 1) else (exit /b 0)
